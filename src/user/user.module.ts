c7006e8d4e04a91a9432bc129cbc367fa2b611f9
zhouming <minerchow@126.com>
2025-02-18 22:15:37 +0800

    update

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-
+import { User } from './entities/user.entity';
 @Module({
+  imports: [User],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable ,Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { RegisterUserDto } from './dto/register-dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class UserService {
+  private logger = new Logger();
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+  async register(user: RegisterUserDto) {
+        
+  }
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
